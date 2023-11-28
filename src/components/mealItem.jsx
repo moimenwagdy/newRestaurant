1dd5a030ae803743e940e18cf4788dc0a9a0f43a
moimenwagdy <moimenwy@gmail.com>
2023-11-28 20:29:49 +0200

    style edit

--- src/components/mealItem.jsx
+++ src/components/mealItem.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { appContext } from "../contextApi/selectedMealsContext";
-import {convertNum} from "../functions/validation";
+import { convertNum } from "../functions/validation";
 export default function MealItem() {
   let { Error, addItem, availbleMeals, isLoading } = useContext(appContext);
 
@@ -12,18 +12,23 @@ export default function MealItem() {
   }
   return availbleMeals.map((meal) => {
     return (
-      <ul className="meal-item" key={meal.id}>
-        <li>
-          <img src={`https://restaurantt.onrender.com/${meal.image}`} alt="MealImage" />
+      <ul id="meals" key={meal.id}>
+        <li className=" meal-item " >
+          <img
+            src={`https://restaurantt.onrender.com/${meal.image}`}
+            alt="MealImage"
+          />
           <h3>{meal.name}</h3>
           <p className="meal-item-price">{convertNum(meal.price)}</p>
           <p className="meal-item-description">{meal.description}</p>
-          <button
-            onClick={() => addItem(meal.id, meal.name, meal.price)}
-            className="button mealButton"
-            type="button ">
-            add to cart
-          </button>
+          <p className="meal-item-actions">
+            <button
+              onClick={() => addItem(meal.id, meal.name, meal.price)}
+              className="button mealButton"
+              type="button ">
+              add to cart
+            </button>
+          </p>
         </li>
       </ul>
     );
